fdb0f7a0943869f47f003ee1064d90370ceccbdd
alejhandro <alejhandro.de@gmail.com>
2018-04-07 14:49:49 -0600

    create product service implementation, and product class content

--- sites/testsite/src/app/models/product.ts
+++ sites/testsite/src/app/models/product.ts
@@ -1,2 +1,8 @@
+
 export class Product {
+    $key: string;
+    name: string;
+    category: string;
+    location: string;
+    price: number;
 }
--- sites/testsite/src/app/services/product.service.ts
+++ sites/testsite/src/app/services/product.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@angular/core';
 
+// importar firebase
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Product } from '../models/product';
+
+
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  productList: AngularFireList<any>;
+  selectProduct: Product = new Product();
 
+  constructor(private firebase: AngularFireDatabase) {
+
+
+  }
+
+  getProducts() {
+    // from firebase, get a list of all elements from 'products' collection
+    this.productList = this.firebase.list('products');
+  }
+
+  insertProducts(product: Product) {
+    // insert a product to database, addint at the end of productlist one element
+    // this.productList.push(product);
+
+    this.productList.push({
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+
+  }
+
+  updateProduct(product: Product) {
+    // update a certain product element in the list
+    // first it is needed to find by a key one product and after that
+    // we pass an object with all elements data to update
+
+    this.productList.update(product.$key, {
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  deleteProduct($key: string) {
+    this.productList.remove($key);
+  }
 }
